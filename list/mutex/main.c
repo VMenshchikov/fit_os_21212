b50266a5dd83c2c847b83bca21a2fae013cfaa81
VMenshchikov <105418201+VMenshchikov@users.noreply.github.com>
2023-12-20 00:53:22 +0700

    переставил локиыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыы

--- list/mutex/main.c
+++ list/mutex/main.c
@@ -48,8 +48,8 @@ void* seeker1(void* arg) {
     for(;;) {
         for (Node* node = storage->first; node->next != NULL; node = node->next) {
             pthread_testcancel();
-            pthread_mutex_lock(&node->next->sync);
             pthread_mutex_lock(&node->sync);
+            pthread_mutex_lock(&node->next->sync);
 
             if (strlen(node->value) < strlen(node->next->value)) s1++;
 
@@ -64,8 +64,8 @@ void* seeker2(void* arg) {
     for(;;) {
         for (Node* node = storage->first; node->next != NULL; node = node->next) {
             pthread_testcancel();
-            pthread_mutex_lock(&node->next->sync);
             pthread_mutex_lock(&node->sync);
+            pthread_mutex_lock(&node->next->sync);
 
             if (strlen(node->value) > strlen(node->next->value)) s2++;
 
@@ -81,8 +81,8 @@ void* seeker3(void* arg) {
     for(;;) {
         for (Node* node = storage->first; node->next != NULL; node = node->next) {
             pthread_testcancel();
-            pthread_mutex_lock(&node->next->sync);
             pthread_mutex_lock(&node->sync);
+            pthread_mutex_lock(&node->next->sync);
 
             if (strlen(node->value) == strlen(node->next->value)) s3++;
 
@@ -98,17 +98,14 @@ int main(int argc, char const *argv[])
     if (argc != 2) return -1;
     const int lenList = atoi(argv[1]);
     Storage* storage = initList(lenList);
-    //printf("1\n");
     pthread_t threads[6];
 
     pthread_create(&threads[0], NULL, swaper, storage);
     pthread_create(&threads[1], NULL, swaper, storage);
     pthread_create(&threads[2], NULL, swaper, storage);
-    //printf("2\n");
     pthread_create(&threads[3], NULL, seeker1, storage);
     pthread_create(&threads[4], NULL, seeker2, storage);
     pthread_create(&threads[5], NULL, seeker3, storage);
-    //printf("3\n");
 
     for (int i = 0; i < 500; i++) {
         printf("Перестановки:%d. Возрастает:%d. Убывает:%d. Равно:%d\n", swap, s1, s2, s3);
@@ -125,29 +122,7 @@ int main(int argc, char const *argv[])
     return 0;
 }
 
-/* void random_nodes_swap(List* list) {
-    assert(list);
-    assert(list->head);
 
-    pthread_mutex_lock(&list->head->sync);
-    ListNode* nodes[3];
-    
-    for (nodes[0] = list->head; nodes[0]->next != NULL; nodes[0] = nodes[1]) {
-        pthread_mutex_lock(&nodes[0]->next->sync);
-        nodes[1] = nodes[0]->next;
-        if (nodes[1]->next != NULL) {
-            pthread_mutex_lock(&nodes[1]->next->sync);
-            nodes[2] = nodes[1]->next;
-            if (nodes[2] && rand() % 2) {
-                list->successfully_swaped_amount++;
-                nodes[0]->next = nodes[2];
-                nodes[1]->next = nodes[2]->next;
-                nodes[2]->next = nodes[1];
-            }
-            pthread_mutex_unlock(&nodes[2]->sync);
-        }
-        pthread_mutex_unlock(&nodes[0]->sync);
-    }
         
     pthread_mutex_unlock(&nodes[0]->sync);
-} */
+} 
